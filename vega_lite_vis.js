519ef5bdbad4c0bd39a7af8ea8f7a7b30228888f
ElyonFunc <elyon@func.vc>
2026-04-22 11:14:02 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,6 @@
+var vg_1 = "daily_cost_bar_chart.vg.json";
+
+vegaEmbed("#bar_chart", vg_1).then(function(result) {
+    
+    //Access the Vega View Instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
